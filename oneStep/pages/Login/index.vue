5be03b9b33e1ff4993798895715dcc9f15571eb8
NikoManiac <niko.sky.lan@gmail.com>
2018-02-07 23:53:58 +0800

    login setting

--- oneStep/pages/Login/index.vue
+++ oneStep/pages/Login/index.vue
@@ -1,93 +1,89 @@
 <template>
-    <form>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex xs12>
         <v-text-field
-            v-model="name"
-            label="Name"
-            :counter="10"
-            
-            data-vv-name="name"
-            required>
-        </v-text-field>
+          name="phone"
+          prepend-icon="phone"
+          label="Enter your password"
+          hint="输入手机号码"
+          v-model="password"
+          min="11"
+          max='11'
+          :append-icon="e1 ? 'visibility' : 'visibility_off'"
+          :append-icon-cb="() => (e1 = !e1)"
+          :type="e1 ? 'password' : 'text'"
+          counter
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Visible</v-subheader>
+      </v-flex>
+      <v-flex xs8>
         <v-text-field
-            v-model="email"
-            label="E-mail"
-            
-            data-vv-name="email"
-            required>
-        </v-text-field>
-        <!-- <v-select
-            :items="items"
-            v-model="select"
-            label="Select"
-            :error-messages="errors.collect('select')"
-            v-validate="'required'"
-            data-vv-name="select"
-            required
-        ></v-select> -->
-        <v-checkbox
-            v-model="checkbox"
-            value="1"
-            label="Option"
-            
-            data-vv-name="checkbox"
-            type="checkbox"
-            required>
-        </v-checkbox>
-
-        <v-btn @click="submit">提交</v-btn>
-        <!-- <v-btn @click="clear">clear</v-btn> -->
-    </form>
+          name="input-10-2"
+          label="Enter your password"
+          hint="At least 8 characters"
+          min="8"
+          :append-icon="e2 ? 'visibility' : 'visibility_off'"
+          :append-icon-cb="() => (e2 = !e2)"
+          value="wqfasds"
+          class="input-group--focused"
+          :type="e2 ? 'password' : 'text'"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Not visible</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="input-10-2"
+          label="Enter your password"
+          hint="At least 8 characters"
+          min="8"
+          :append-icon="e3 ? 'visibility' : 'visibility_off'"
+          :append-icon-cb="() => (e3 = !e3)"
+          value="wqfasds"
+          class="input-group--focused"
+          :type="e3 ? 'password' : 'text'"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Error</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="input-10-2"
+          label="Enter your password"
+          hint="At least 8 characters"
+          min="8"
+          :append-icon="e4 ? 'visibility' : 'visibility_off'"
+          :append-icon-cb="() => (e4 = !e4)"
+          value="Pa"
+          :rules="[() => ('The email and password you entered don\'t match')]"
+          error
+          :type="e4 ? 'password' : 'text'"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
     name: 'login',
-    $_veeValidate: {
-      validator: 'new'
-    },
-
-    data: () => ({
-      name: '',
-      email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4'
-      ],
-      checkbox: null,
-      dictionary: {
-        attributes: {
-          email: 'E-mail Address'
-          // custom attributes
-        },
-        custom: {
-          name: {
-            required: () => 'Name can not be empty',
-            max: 'The name field may not be greater than 10 characters'
-            // custom messages
-          },
-          select: {
-             required: 'Select field is required'
-          }
-        }
-      }
-    }),
-
-    // mounted () {
-    //   this.$validator.localize('en', this.dictionary)
-    // },
-
-    methods: {
-      submit () {
-        this.$validator.validateAll()
-      },
-      clear () {
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = null
-        this.$validator.reset()
+    data () {
+      return {
+        e1: false,
+        e2: false,
+        e3: false,
+        e4: false,
+        password: 'Password'
       }
     }
   }
