0eb92452f8e3eea344e80684346d6dee1111c7f7
NikoManiac <niko.sky.lan@gmail.com>
2018-02-08 00:46:42 +0800

    updata vuetify and setting login page and prepare tencent sms

--- oneStep/pages/Login/index.vue
+++ oneStep/pages/Login/index.vue
@@ -1,78 +1,72 @@
 <template>
-  <v-container fluid>
-    <v-layout row>
-      <v-flex xs12>
-        <v-text-field
-          name="phone"
-          prepend-icon="phone"
-          label="Enter your password"
-          hint="输入手机号码"
-          v-model="password"
-          min="11"
-          max='11'
-          :append-icon="e1 ? 'visibility' : 'visibility_off'"
-          :append-icon-cb="() => (e1 = !e1)"
-          :type="e1 ? 'password' : 'text'"
-          counter
-        ></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs4>
-        <v-subheader>Visible</v-subheader>
-      </v-flex>
-      <v-flex xs8>
-        <v-text-field
-          name="input-10-2"
-          label="Enter your password"
-          hint="At least 8 characters"
-          min="8"
-          :append-icon="e2 ? 'visibility' : 'visibility_off'"
-          :append-icon-cb="() => (e2 = !e2)"
-          value="wqfasds"
-          class="input-group--focused"
-          :type="e2 ? 'password' : 'text'"
-        ></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs4>
-        <v-subheader>Not visible</v-subheader>
-      </v-flex>
-      <v-flex xs8>
-        <v-text-field
-          name="input-10-2"
-          label="Enter your password"
-          hint="At least 8 characters"
-          min="8"
-          :append-icon="e3 ? 'visibility' : 'visibility_off'"
-          :append-icon-cb="() => (e3 = !e3)"
-          value="wqfasds"
-          class="input-group--focused"
-          :type="e3 ? 'password' : 'text'"
-        ></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs4>
-        <v-subheader>Error</v-subheader>
-      </v-flex>
-      <v-flex xs8>
-        <v-text-field
-          name="input-10-2"
-          label="Enter your password"
-          hint="At least 8 characters"
-          min="8"
-          :append-icon="e4 ? 'visibility' : 'visibility_off'"
-          :append-icon-cb="() => (e4 = !e4)"
-          value="Pa"
-          :rules="[() => ('The email and password you entered don\'t match')]"
-          error
-          :type="e4 ? 'password' : 'text'"
-        ></v-text-field>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <v-container fluid>
+        <!-- <div>
+            <v-tabs v-model="active">
+                <v-tabs-bar class="cyan" dark>
+                    <v-tabs-item
+                        v-for="tab in tabs"
+                        :key="tab"
+                        :href="'#' + tab"
+                        ripple>
+                        {{ tab.slice(-1) }}
+                    </v-tabs-item>
+                    <v-tabs-slider color="yellow"></v-tabs-slider>
+                </v-tabs-bar>
+                <v-tabs-items>
+                    <v-tabs-content
+                        v-for="tab in tabs"
+                        :key="tab"
+                        :id="tab">
+                        <v-card flat>
+                            <v-card-text>{{ text }}</v-card-text>
+                        </v-card>
+                    </v-tabs-content>
+                </v-tabs-items>
+            </v-tabs>
+        </div> -->
+        <v-layout row>
+            <v-flex xs3>
+                <v-subheader>手机</v-subheader>
+            </v-flex>
+            <v-flex xs9>
+                <v-text-field
+                name="phone"
+                label='请输入手机号'
+                hint="输入手机号码"
+                v-model="phone"
+                min="11"
+                max='11'
+                class="input-group--focused"
+                ></v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex xs3>
+                <v-subheader>密码</v-subheader>
+            </v-flex>
+            <v-flex xs5>
+                <v-text-field
+                name="pwd"
+                label='验证码'
+                hint="至少输入8位密码"
+                min="8"     
+                value=""
+                ></v-text-field>
+            </v-flex>
+            <v-flex xs4>
+                <div>
+                    <v-btn depressed>获取验证码</v-btn>
+                </div>
+            </v-flex>
+        </v-layout>
+        <v-layout>
+            <v-flex xs12>
+                <div>
+                    <v-btn depressed large>提交</v-btn>
+                </div>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 <script>
 export default {
@@ -83,7 +77,17 @@ export default {
         e2: false,
         e3: false,
         e4: false,
-        password: 'Password'
+        phone: '',
+        tabs: ['登录', '注册'],
+        active: null,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        
+      }
+    },
+    methods: {
+      next () {
+        const active = parseInt(this.active)
+        this.active = (active < 2 ? active + 1 : 0).toString()
       }
     }
   }
