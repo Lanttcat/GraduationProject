cf27da02ae7af4cca0a45df2a61dd5f50d15250f
NikoManiac <niko.sky.lan@gmail.com>
2018-02-26 15:52:34 +0800

    blockchain note && puppeteer get info from mafengwo

--- oneScrapyHeadless/index.js
+++ oneScrapyHeadless/index.js
@@ -1,5 +1,9 @@
 const puppeteer = require('puppeteer');
 
+function getList() {
+
+}
+
 (async () => {
     let allJDInfo = [];
     const browser = await puppeteer.launch({
@@ -7,41 +11,60 @@ const puppeteer = require('puppeteer');
     });
     const page = await browser.newPage();
     await page.goto('http://www.mafengwo.cn/jd/10575/gonglve.html');
-    await page.click('.pg-next');
 
-    let currentPageJDList = await page.evaluate(() => {
-        let list = [...document.querySelectorAll('.row-allScenic .scenic-list li')];
-        return list.map(el => {
-            return {
-                href: el.querySelector('a').href.trim(),
-                imgSrc: el.querySelector('div img').src.trim(),
-                title: el.querySelector('a h3').innerHTML
+    async function getAllScenicSpot() {
+        let allScenicSpot = [];        
+        for (let i = 0;i < 16;i++) {
+            console.log('当前第：' + i + '页');
+            try {
+                let list = await page.evaluate(() => {
+                    return new Promise((resolve, reject) => {
+                        let xhr = new XMLHttpRequest();
+                        
+                        let data = "sAct=KMdd_StructWebAjax|GetPoisByTag&iMddid=10575&iTagId=0&iPage=3";
+            
+                        xhr.open('post', '/ajax/router.php', true);
+                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+                        xhr.setRequestHeader('Accept', 'application/json, text/javascript, */*; q=0.01');
+                        xhr.send(encodeURI(data));
+            
+                        xhr.onreadystatechange = () => {
+                            if(xhr.readyState === 4) {
+                                if (xhr.status >= 200 && xhr.status <= 300) {
+                                    let result = []
+                                    let data = JSON.parse(xhr.responseText).data.list;
+                                    let element = document.createElement('div');
+                                    element.innerHTML = data;
+                                    for (let i = 0;i < element.children.length;i++) {
+                                        let obj = {};
+                                        obj.title = element.children[i].querySelector('a').title;
+                                        obj.ssHref = element.children[i].querySelector('a').href;
+                                        obj.imgSrc = element.children[i].querySelector('img').src;
+                                        console.log(obj);
+                                        result.push(obj);
+                                    }
+                                    resolve(result);
+                                }
+                                else {
+                                    resolve('错误');
+                                }
+                            }
+                        }
+                    });
+                });
+                console.log('cdcdcd');
+                allScenicSpot.push(...list);
             }
-        })
-    });
-    var i = 1;
-    let nextPage;
-    // async function test() {
-    //     try {
-    //         do {
-    //             console.log(i++ + '页');        
-    //             await allJDInfo.push(currentPageJDList);
-    //             nextPage = await page.$('.pg-next');
-    //             await nextPage.click();
-    //             // if (i===10){
-    //             //     break;
-    //             // }
-    //             // await page.waitFor(1000);
-    //         } while (nextPage);
-    //     }
-    //     catch (e) {
-    //         console.log('错误'+e);
-    //     }
-    // }
-    // test();
-    // test().then(() => console.log(allJDInfo))
-    // var name = await page.$('.pg-next');
-    // await page.click('.pg-next', {delay: 1000});
-    console.log(currentPageJDList);
+            catch (e) {
+                console.log(e);
+                await browser.close();
+            }
+        }
+        return allScenicSpot;
+    }
+
+    await getAllScenicSpot().then((res) => {console.log(res)});
+
+    // 关闭浏览器
     await browser.close();
 })();
